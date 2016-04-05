6348cbbfc1ef53847cd51faea391fcdf5af06c0e
Joel Brewer <joelbrewer01@gmail.com>
2016-04-05 20:24:30 +0000

    save affiliate ID and customer ID to cookies if they exist in the query params

--- Observer/PageLoadObserver.php
+++ Observer/PageLoadObserver.php
@@ -8,21 +8,28 @@ error_reporting(E_ALL);
 
 class PageLoadObserver implements ObserverInterface {
   public function execute(\Magento\Framework\Event\Observer $observer) {
-		$url = 'https://toorapi.ngrok.io/test';
-		$data = array('key1' => 'Someone has visited Pref*it!');
+		$affiliate_id = $customer_id = null;
+		$affiliate_cookie_name = "brewerdigital_affiliate_affiliate_id";
+		$customer_cookie_name  = "brewerdigital_affiliate_customer_id";
+		$seconds_in_day = 86400;
+		$cookie_length_in_days = 30;
+    $cookie_length_in_seconds = $seconds_in_day * $cookie_length_in_days;
 
-		// use key 'http' even if you send the request to https://...
-		$options = array(
-				'http' => array(
-						'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-						'method'  => 'POST',
-						'content' => http_build_query($data)
-				)
-		);
-		$context  = stream_context_create($options);
-		$result = file_get_contents($url, false, $context);
+		$params = $observer->getEvent()->getRequest()->getParams();
+
+		if (isset($params["a"])) {
+ 			$affiliate_id = $params["a"];
+		  setcookie($affiliate_cookie_name, $affiliate_id, time() + $cookie_length_in_seconds, "/");
+		}
+
+		if (isset($params["cid"])) {
+			$customer_id = $params["cid"];
+		  setcookie($customer_cookie_name, $customer_id, time() + $cookie_length_in_seconds, "/");
+		}
+		
     echo '<pre>';
-    var_dump($result);
+    var_dump($affiliate_id);
+    var_dump($customer_id);
     echo '</pre>';
 	}
 }
